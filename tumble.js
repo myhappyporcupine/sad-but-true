1a7f1355ff2e5a8a1caf7ea9a2d3d4de9ea251dd
Ran Shoham <my.happy.porcupine@gmail.com>
2018-05-19 16:15:16 +0300

    Added variables and transforms to tumble.js

--- tumble.js
+++ tumble.js
@@ -2,6 +2,21 @@
 	const canvas = document.getElementById('tumble-canvas');
 	const ctx = canvas.getContext('2d');
 
+	// Variables
+	// Head
+	const headCenterX = 100;
+	const headCenterY = 100;
+	const headRadius = 50;
+	const foreheadHeight = headRadius / 5;
+	const headRotation = 0;
+	// Eyes
+	const browHeight = 2/5 * headRadius;
+	const eyeRadius = headRadius / 10;
+	// Nose
+	const noseWidth = headRadius / 5;
+	const noseHeight = noseWidth / 2;
+	const noseOffsetFromTop = 3/5 * headRadius;
+
 	// Styles
 	ctx.strokeStyle = '#824993';
 	ctx.fillStyle = '#edddad';
@@ -14,14 +29,19 @@
 	ctx.fill();
 	ctx.stroke();
 
+	// Transform for Tumble's Head
+	ctx.save();
+	ctx.translate(headCenterX, headCenterY);
+	ctx.rotate(headRotation);
+
 	// Tumble's Head
 	ctx.beginPath();
-	ctx.moveTo(150, 100);
-	ctx.arc(100, 100, 50, 0, Math.PI);
-	ctx.lineTo(50, 50);
-	ctx.lineTo(75, 90);
-	ctx.lineTo(125, 90);
-	ctx.lineTo(150, 50);
+	ctx.moveTo(headRadius, 0);
+	ctx.arc(0, 0, headRadius, 0, Math.PI);
+	ctx.lineTo(-headRadius, -headRadius);
+	ctx.lineTo(-headRadius/2, -foreheadHeight);
+	ctx.lineTo(headRadius/2, -foreheadHeight);
+	ctx.lineTo(headRadius, -headRadius);
 	ctx.closePath();
 	ctx.fill();
 	ctx.stroke();
@@ -29,23 +49,26 @@
 	// Tumble's Eyes
 	// Left
 	ctx.beginPath();
-	ctx.arc(75, 120, 5, 0, 2*Math.PI);
+	ctx.arc(headRadius/2, browHeight, eyeRadius, 0, 2*Math.PI);
 	ctx.closePath();
 	ctx.fill();
 	ctx.stroke();
 	// Right
 	ctx.beginPath();
-	ctx.arc(125, 120, 5, 0, 2*Math.PI);
+	ctx.arc(-headRadius/2, browHeight, eyeRadius, 0, 2*Math.PI);
 	ctx.closePath();
 	ctx.fill();
 	ctx.stroke();
 
 	// Tumble's Nose
 	ctx.beginPath();
-	ctx.moveTo(95, 130);
-	ctx.lineTo(105, 130);
-	ctx.lineTo(100, 135);
+	ctx.moveTo(-noseWidth/2, noseOffsetFromTop);
+	ctx.lineTo(noseWidth/2, noseOffsetFromTop);
+	ctx.lineTo(0, noseOffsetFromTop + noseHeight);
 	ctx.closePath();
 	ctx.fill();
 	ctx.stroke();
+
+	// Restore from Transform of Tumble's Head
+	ctx.restore();
 })();
